858646a418b297b703404c873325e46d6d887b1e
Alexandre Gama <alexandre.gama.lima@gmail.com>
2014-05-15 19:38:02 -0300

    Pagina de Produtos e css decente

--- src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,5 +1,7 @@
 package br.com.caelum.notasfiscais.mb;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.caelum.notasfiscais.dao.ProdutoDao;
@@ -10,18 +12,36 @@ public class ProdutoBean {
 
 	private Produto produto = new Produto();
 	
+	private List<Produto> produtos;
+	
 	public void grava() {
 		ProdutoDao produtoDao = new ProdutoDao();
-		produtoDao.adiciona(produto);
+		if (produto.getId() == null) {
+			produtoDao.adiciona(produto);
+		} else {
+			produtoDao.atualiza(produto);
+		}
 		limpaFormulario();
 	}
 
-	private void limpaFormulario() {
-		this.produto = new Produto();
+	public List<Produto> getProdutos() {
+		if (this.produtos == null) {
+			this.produtos = new ProdutoDao().listaTodos();
+		}
+		return produtos;
 	}
 	
 	public Produto getProduto() {
 		return produto;
 	}
 	
+	private void limpaFormulario() {
+		this.produto = new Produto();
+		this.produtos = null;
+	}
+
+	public void remove(Produto produto) {
+		new ProdutoDao().remove(produto);
+		limpaFormulario();
+	}
 }
